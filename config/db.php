830d005665c4c98605caf9909ac35edb5967a250
Amarilis Camargo <amariliscamargo@gmail.com>
2021-12-11 19:15:40 -0300

    iniciando o projeto

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:server=db;port=3306;dbname=medbeauty',
+    'username' => 'amarilis',
+    'password' => '1qaz2wsx',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
