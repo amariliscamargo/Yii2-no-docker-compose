fc86037f2b272b1ca5dca86366a84c14d01db6d6
Amarilis Camargo <amariliscamargo@gmail.com>
2021-12-12 18:23:39 -0300

    add mailhog

--- config/web.php
+++ config/web.php
@@ -28,10 +28,15 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => null,
+                'host' => 'mailhog',
+                'port' => '1025',
+                'username' => 'null',
+                'password' => 'null',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
